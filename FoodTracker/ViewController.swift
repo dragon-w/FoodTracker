7e68997eda4fec1b4c48e89e4334d2f20b49311e
dragon-w <dragon-w@dragon-wdemacbook-pro.local>
2016-07-14 18:19:52 +0800

    增加评星功能

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -15,12 +15,12 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         
         
         super.viewDidLoad()
-        
         // Handle the text field’s user input through delegate callbacks.
         nameTextField.delegate = self
         
@@ -74,12 +74,5 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         
         presentViewController(imagePickerController, animated: true, completion: nil)
     }
-
-    
-    @IBAction func setDefaultLabelText(sender: UIButton) {
-        
-        mealNameLabel.text = "Default Text"
-    }
-    
 }
 
